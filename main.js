20ea0df6b9c2806377078673d3d99188c594a68f
Varkey Josu <varkeyjosu2003@gmail.com>
2024-03-06 20:12:41 +0530

    test2

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-
+/*
 const accordians = document.querySelectorAll('.accordian')
 accordians.forEach(accordian =>{
   const answer=accordian.querySelector('.answer');
@@ -6,3 +6,22 @@ accordians.forEach(accordian =>{
                              answer.classList.toggle('active');
                              })
 })
+*/
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
